8d2a1f202756ef3d298abd17666c0b2282097459
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-08-30 11:20:12 -0300

    Aplicando o IComparable

---
+++ Interfaces/IComparable/AprendendoIComparable/AprendendoIComparable/Entities/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Employee : IComparable
+    {
+        public string name { get; set; }
+        public double salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            name = vect[0];
+            salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return $"{name},{salary}";
+        }
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Employee");
+            }
+
+            Employee other = obj as Employee;
+
+            return name.CompareTo(other.name); // salary.CompareTo(other.salary);
+        }
+    }
+}
---
+++ Interfaces/IComparable/AprendendoIComparable/AprendendoIComparable/Program.cs
@@ -0,0 +1,30 @@
+﻿using Course;
+
+namespace AprendendoIComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "teste.csv";
+
+            List<Employee> list = new List<Employee>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    list.Add(new Employee(sr.ReadLine()));
+                }
+
+                list.Sort(); // Só funciona por causa do IComparable e seu método CompareTo.
+            }
+
+            foreach (Employee emp in list)
+                Console.WriteLine(emp);
+
+
+
+        }
+    }
+}
